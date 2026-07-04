595e0b3c187a016ebfc90e6981054813d3ad9081
Venko Venkov <150695843+vmvenkov@users.noreply.github.com>
2026-07-04 21:14:48 +0300

    Create schemas.py

---
+++ api/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+
+class ClaimRequest(BaseModel):
+    description: str
+
+
+class PredictionResponse(BaseModel):
+    insurance_type: str
+    insurance_confidence: float
+
+    claim_type: str
+    claim_confidence: float
+
+    severity: str
+    severity_confidence: float
+
+    recommendation: str
